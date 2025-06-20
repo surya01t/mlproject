d2207b242809299f202301b69b09a182258bd9a8
Suryansh Tripathi <suriya001thesingham@gmail.com>
2025-06-20 15:19:48 +0530

    Hyperparameter Training

--- src/exception.py
+++ src/exception.py
@@ -1,19 +1,22 @@
 import sys
 import logging
 
-def error_message_details(error,error_detail:sys):
-    _,_,exc_tb=error_detail.exc_info()
-    file_name=exc_tb.tb_frame.f_code.co_filename
-    error_message="Error occured in python script name[{0}] line number [{1}] error message [{2}]".format(
-     file_name,exc_tb.tb_lineno,str(error)
-    )
+import sys
+import traceback
+
+def error_message_details(error, error_detail: sys):
+    _, _, exc_tb = error_detail.exc_info()
+    if exc_tb is not None:
+        file_name = exc_tb.tb_frame.f_code.co_filename
+        line_number = exc_tb.tb_lineno
+        return f"Error occurred in script: {file_name} at line {line_number} — Error: {str(error)}"
+    else:
+        return f"Error: {str(error)}"
 
 class CustomException(Exception):
-    def __init__(self,error_message,error_detail:sys):
-        super().__init__(error_message)
-        self.error_message=error_message_details(error_message,error_detail=error_detail) 
+    def __init__(self, error_message, error_detail: sys):
+        super().__init__(str(error_message))
+        self.error_message = error_message_details(error_message, error_detail)
 
     def __str__(self):
-        return self.error_message
-    
-
+        return str(self.error_message)
--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,8 @@ import pandas as pd
 from src.exception import CustomException
 import dill
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
+
 
 def save_object(file_path, obj):
     """
@@ -24,18 +26,28 @@ def save_object(file_path, obj):
 
 
 
-def evaluate_models(X_train, y_train, X_test, y_test, models: dict):
+def evaluate_models(X_train, y_train, X_test, y_test, models: dict,param):
     try:
         report = {}
 
-        for name, model in models.items():
-            model.fit(X_train, y_train)
-            preds = model.predict(X_test)
-            score = r2_score(y_test, preds)
+        for i in range(len(list(models))):
+            model=list(models.values())[i]
+            model_name = list(models.keys())[i]
+            para = param.get(model_name, {})
 
-            # Don't print here — just store
-            report[name] = score
+            gs=GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
 
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
+
+            y_train_pred=model.predict(X_train)
+            y_test_pred=model.predict(X_test)
+
+            train_model_score=r2_score(y_train,y_train_pred)
+            test_model_score=r2_score(y_test,y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
         return report
     except Exception as e:
         raise CustomException(e, sys)
