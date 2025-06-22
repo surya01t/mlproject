2fb4093e987d4e056c544b9ae152efbadb701cde
Suryansh Tripathi <suriya001thesingham@gmail.com>
2025-06-22 22:51:45 +0530

    final commit

--- app.py
+++ app.py
@@ -38,8 +38,8 @@ def predict_datapoint():
 
         predict_pipeline=PredictPipeline()
         results=predict_pipeline.predict(pred_df)
-        return render_template('home.html',results=results[0])
-    
+        return render_template('home.html',results=min(round(float(results[0]),2),100))
+
 
 if __name__=="__main__":
     app.run(host="0.0.0.0",debug=True)
--- application.py
+++ application.py
@@ -38,7 +38,7 @@ def predict_datapoint():
 
         predict_pipeline=PredictPipeline()
         results=predict_pipeline.predict(pred_df)
-        return render_template('home.html',results=results[0])
+        return render_template('home.html',results=min(round(float(results[0]),2),100))
     
 
 if __name__=="__main__":
